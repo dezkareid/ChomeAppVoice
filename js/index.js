b684a8023b40459723c687d65f7cd336945259a9
dezkareid <elmaildeldezkareid@gmail.com>
2014-08-18 00:10:23 -0500

    Initial
    Initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var recognition ;
+function saluda () {
+  var msg = new SpeechSynthesisUtterance();
+  msg.text = 'welcome to gdg monterrey';
+
+  
+  window.speechSynthesis.speak(msg);
+}
+
+function escucha () {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = "en";
+  recognition.onresult = function (event) {
+
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+          
+        if(event.results[i][0].transcript.trim()=="hello"){
+          recognition.stop();
+          saluda();
+        }
+         console.log(event.results[i][0].transcript.trim());
+      }
+    }
+  }
+
+  recognition.onend = function() { console.log("Termine"); };
+  recognition.start();
+
+}
+
+
+
+window.onload += escucha();
